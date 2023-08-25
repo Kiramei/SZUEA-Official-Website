4d42944275f6b6ff0e5b7f36b40d2e5f0348bbe7
Kiramei <1710623513@qq.com>
2023-08-25 10:10:24 +0800

    v1.0
    Create the static mod for our website.

---
+++ wp-admin/customize.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=/wp-login.php?redirect_to=https%3A%2F%2Fwww.szuea.com%2Fwp-admin%2Fcustomize.php%3Fautofocus%255Bsection%255D%3Dlogin_designer__section--templates%26return%3Dhttps%253A%252F%252Fwww.szuea.com%252Fwp-admin%252Findex.php%26url%3Dhttps%253A%252F%252Fwww.szuea.com%252Flogin-designer%252F%26simply_static_page%3D71&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "/wp-login.php?redirect_to=https%3A%2F%2Fwww.szuea.com%2Fwp-admin%2Fcustomize.php%3Fautofocus%255Bsection%255D%3Dlogin_designer__section--templates%26return%3Dhttps%253A%252F%252Fwww.szuea.com%252Fwp-admin%252Findex.php%26url%3Dhttps%253A%252F%252Fwww.szuea.com%252Flogin-designer%252F%26simply_static_page%3D71&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="/wp-login.php?redirect_to=https%3A%2F%2Fwww.szuea.com%2Fwp-admin%2Fcustomize.php%3Fautofocus%255Bsection%255D%3Dlogin_designer__section--templates%26return%3Dhttps%253A%252F%252Fwww.szuea.com%252Fwp-admin%252Findex.php%26url%3Dhttps%253A%252F%252Fwww.szuea.com%252Flogin-designer%252F%26simply_static_page%3D71&reauth=1">/wp-login.php?redirect_to=https%3A%2F%2Fwww.szuea.com%2Fwp-admin%2Fcustomize.php%3Fautofocus%255Bsection%255D%3Dlogin_designer__section--templates%26return%3Dhttps%253A%252F%252Fwww.szuea.com%252Fwp-admin%252Findex.php%26url%3Dhttps%253A%252F%252Fwww.szuea.com%252Flogin-designer%252F%26simply_static_page%3D71&reauth=1</a></p>
+	</body>
+</html>
